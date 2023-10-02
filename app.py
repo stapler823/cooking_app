dfc87ec27a125949fa820ba610ca2555e8cf6299
Stapler823 <info@stapler823.com>
2023-10-03 01:22:19 +0900

    gitignoreに.envを追加。ChatGPTのサンプルを作成

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from dotenv import load_dotenv
+import openai
 
 app = Flask(__name__)
 
+load_dotenv()
+
+
 @app.route('/')
 def hello():
-    return "Hello World"
+    response = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    messages = [
+        {"role": "system", "content": "あなたは役にたつアシスタントです"},
+        {"role": "user", "content": "「覆水盆に返らず」これを英訳してください。"}
+    ]
+  )
+    return response
 
 app.run()
