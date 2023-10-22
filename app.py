f907d5cc134e56710a9fb9cb9f1bd49f4ad6e80d
Stapler823 <info@stapler823.com>
2023-10-22 18:45:07 +0900

    チャット機能の作成

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
-import openai
 from llama_index import GPTListIndex
 from pathlib import Path
 from llama_index import download_loader
 import logging
 import sys
-from llama_index import QuestionAnswerPrompt
-# from gpt_index import download_loader
-
-
+from llama_index import QuestionAnswerPrompt,LLMPredictor,ServiceContext, StorageContext,load_index_from_storage, VectorStoreIndex
+from langchain.chat_models import ChatOpenAI
+import os
+from llama_index.vector_stores.faiss import FaissVectorStore
+import faiss 
+import markdown
 
 app = Flask(__name__)
 
@@ -17,30 +18,107 @@ load_dotenv()
 
 logging.basicConfig(filename='sample.log', level=logging.DEBUG, force=True)
 
-@app.route('/')
-def hello():
-    CJKPDFReader = download_loader("CJKPDFReader")
-    loader = CJKPDFReader()
-    documents = loader.load_data(file='./input/recipe_test.pdf')
+def generate_text(prompt):
+   llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo'))
 
-    list_index = GPTListIndex.from_documents(documents)
-    QA_PROMPT="""
-      あなたは世界中で信頼されている専門的なQ&Aシステムです。
-      提供されたコンテキスト情報を使用してクエリに回答してください。
-      ---------------------
-      {context_str}
-      ---------------------
-      この情報を踏まえて、次の質問に回答してください: {query_str}
-    """
-    # QA_PROMPT="""
-    #   てすと
-    # """
-    query_engine = list_index.as_query_engine(text_qa_template=QuestionAnswerPrompt(QA_PROMPT))
+   service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
 
-    response = query_engine.query("じゃがいも料理に使う材料と分量を教えて")
-    print(response)
-    response = "test"
-    return response
+   QA_PROMPT_TMPL = (
+    "あなたは料理研究家です。"
+    "以下の料理のレシピ情報をコンテキスト情報として与えます。 \n"
+    "---------------------\n"
+    "{context_str}"
+    "\n---------------------\n"
+    "この情報を参照して次の質問に合致する料理の名前・材料・作り方を一つ教えてください: {query_str}\n"
+    "その際、回答の形式は下記としてください。\n"
+    "■料理名\n"
+    "■材料\n"
+    "■作り方\n"
+    )
+   QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
+   path = './storage'
+   is_dir = os.path.isdir(path)
+
+   # インデックスが存在する時は既存のインデックスを使う
+   if is_dir:
+      vector_store = FaissVectorStore.from_persist_dir("./storage")
+      storage_context = StorageContext.from_defaults(vector_store=vector_store, persist_dir="./storage")
+      index = load_index_from_storage(storage_context=storage_context)
+    # インデックスが存在しない時は新規にインデックスを作成する
+   else:
+      # PDFからインデックスを作成
+      CJKPDFReader = download_loader("CJKPDFReader")
+      loader = CJKPDFReader()
+      documents = loader.load_data(file='./input/recipe_test.pdf')
+
+      #dimension of text-ada-embedding-002
+      d = 1536
+
+      # コサイン類似度
+      faiss_index = faiss.IndexFlatIP(d)
+
+      vector_store = FaissVectorStore(faiss_index=faiss_index)
+      storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+      # APIを実行し、Faissのベクター検索ができるようにする
+      index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, service_context=service_context)
+      #save index to disk
+      index.storage_context.persist()
+
+   engine = index.as_query_engine(text_qa_template=QA_PROMPT)
+   response = engine.query(prompt)
+   return response.response
+
+@app.route('/', methods={'GET', 'POST'})
+def home():
+   # フォームに質問を入力して、「送信」を押下したときは
+   # Chat Completion APIにリクエストして、レスポンスを格納する。
+   if request.method == 'POST':
+      prompt = request.form['user_input']
+      text = markdown.markdown(generate_text(prompt)).replace('\n', '<br />')
+   else:
+      #初期状態では何もしない
+      prompt = ""
+      text = ""
+
+   return f'''
+      <form method="post">
+        <label for="user_input">質問を入力してください:</label><br /><br />
+        <input type="text" id="user_input" name="user_input">
+        <input type="submit" value="送信">
+      </form>
+      <p>質問:</p>
+      <p>{prompt}</p>
+      <br />
+      <p>回答:</p>
+      <p>{text}</p>
+   '''
+
+
+# @app.route('/')
+# def hello():
+#     CJKPDFReader = download_loader("CJKPDFReader")
+#     loader = CJKPDFReader()
+#     documents = loader.load_data(file='./input/recipe_test.pdf')
+
+#     list_index = GPTListIndex.from_documents(documents)
+#     QA_PROMPT="""
+#       あなたは世界中で信頼されている専門的なQ&Aシステムです。
+#       提供されたコンテキスト情報を使用してクエリに回答してください。
+#       ---------------------
+#       {context_str}
+#       ---------------------
+#       この情報を踏まえて、次の質問に回答してください: {query_str}
+#     """
+#     # QA_PROMPT="""
+#     #   てすと
+#     # """
+#     query_engine = list_index.as_query_engine(text_qa_template=QuestionAnswerPrompt(QA_PROMPT))
+
+#     response = query_engine.query("じゃがいも料理に使う材料と分量を教えて")
+#     print(response)
+#     response = "test"
+#     return response
 
 if __name__ == "__main__":
   app.run()
